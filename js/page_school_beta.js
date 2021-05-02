ecb73f3fc8728a2f1ab6ed6a0c892b6d2edd6862
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-05-02 17:25:32 -0400

    Create page_school_beta.js

---
+++ js/page_school_beta.js
@@ -0,0 +1,110 @@
+const sessions = require('../data/sessions');
+const fUtil = require('../fileUtil');
+const stuff = require('./info');
+
+function toAttrString(table) {
+	return typeof (table) == 'object' ? Object.keys(table).filter(key => table[key] !== null).map(key =>
+		`${encodeURIComponent(key)}=${encodeURIComponent(table[key])}`).join('&') : table.replace(/"/g, "\\\"");
+}
+function toParamString(table) {
+	return Object.keys(table).map(key =>
+		`<param name="${key}" value="${toAttrString(table[key])}">`
+	).join(' ');
+}
+function toObjectString(attrs, params) {
+	return `<object id="obj" ${Object.keys(attrs).map(key =>
+		`${key}="${attrs[key].replace(/"/g, "\\\"")}"`
+	).join(' ')}>${toParamString(params)}</object>`;
+}
+
+module.exports = function (req, res, url) {
+	if (req.method != 'GET') return;
+	const query = url.query;
+
+	var attrs, params, title;
+	switch (url.pathname) {
+		case '/cc': {
+			title = 'Character Creator';
+			attrs = {
+				data: process.env.SWF_URL + '/cc.swf', // data: 'cc_.swf',
+				type: 'application/x-shockwave-flash', id: 'char_creator', width: '100%', height: '100%',
+			};
+			params = {
+				flashvars: {
+					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>',
+					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'original_asset_id': query['id'] || null,
+					'themeId': 'business', 'ut': 60, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go',
+					'm_mode': 'school', 'isLogin': 'Y', 'isEmbed': 1, 'ctc': 'go', 'tlang': 'en_US',
+				},
+				allowScriptAccess: 'always',
+				movie: process.env.SWF_URL + '/cc.swf', // 'http://localhost/cc.swf'
+			};
+			break;
+		}
+
+		case '/cc_browser': {
+			title = 'Character Creator Browser';
+			attrs = {
+				data: process.env.SWF_URL + '/cc_browser.swf', // data: 'cc_browser_.swf',
+				type: 'application/x-shockwave-flash', id: 'char_creator', width: '100%', height: '100%',
+			};
+			params = {
+				flashvars: {
+					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>',
+					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'original_asset_id': query['id'] || null,
+					'themeId': 'business', 'ut': 60, 'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': 'go',
+					'm_mode': 'school', 'isLogin': 'Y', 'isEmbed': 1, 'ctc': 'go', 'tlang': 'en_US',
+				},
+				allowScriptAccess: 'always',
+				movie: process.env.SWF_URL + '/cc_browser.swf', // 'http://localhost/cc_browser.swf'
+			};
+			break;
+		}
+		
+		case '/videomaker/full': {
+			let presave = query.movieId && query.movieId.startsWith('m') ? query.movieId :
+				`m-${fUtil[query.noAutosave ? 'getNextFileId' : 'fillNextFileId']('movie-', '.xml')}`;
+			title = 'Import/edit video on the Legacy Video Maker';
+			attrs = {
+				data: process.env.SWF_URL + '/go_full.swf',
+				type: 'application/x-shockwave-flash', width: '100%', height: '100%',
+			};
+			params = {
+				flashvars: {
+					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'isEmbed': 1, 'ctc': 'go',
+					'ut': 23, 'u_info_school': 'OjI6c2xoVVM3MWJIX05DMnA4cmRBQ2dFd3JvNE5xc2JEc2o4UFB2X1dVd2Eya2RPQisxVTl4d3ZHZHJPYnI4QURFNENWMjNkYm12WFdlUGxLT0g0OG4rekF5ajZhWGRGVTlocmJ4S1JhSWhCVXJlTF9BbXdyQUp3PQ==',
+				  'bs': 'default', 'appCode': 'go', 'page': '', 'siteId': '13', 'userId': '0DyHqK6Yj9dM', 'role': 'student','lid': 13, 'isLogin': 'Y',
+					'clientThemePath': process.env.CLIENT_URL + '/<client_theme>', 'themeId': 'business', 'tlang': 'en_US',
+					'presaveId': presave, 'goteam_draft_only': 1, 'isWide': 1, 'nextUrl': '/dashboard/videos',
+				},
+				allowScriptAccess: 'always',
+			};
+			sessions.set({ movieId: presave }, req);
+			break;
+		}
+
+		case '/player': {
+			title = 'Video Player';
+			attrs = {
+				data: process.env.SWF_URL + '/player.swf',
+				type: 'application/x-shockwave-flash', width: '100%', height: '100%',
+			};
+			params = {
+				flashvars: {
+					'apiserver': '/', 'storePath': process.env.STORE_URL + '/<store>', 'ut': 60,
+					'autostart': 1, 'isWide': 1, 'clientThemePath': process.env.CLIENT_URL + '/<client_theme>',
+				},
+				allowScriptAccess: 'always',
+			};
+			break;
+		}
+
+		default:
+			return;
+	}
+	res.setHeader('Content-Type', 'text/html; charset=UTF-8');
+	Object.assign(params.flashvars, query);
+	res.end(`<script>document.title='${title}',flashvars=${JSON.stringify(params.flashvars)}</script><body style="margin:0px">${toObjectString(attrs, params)
+		}</body>${stuff.pages[url.pathname] || ''}`);
+	return true;
+}
